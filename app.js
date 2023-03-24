f65cfa510c368fdbf673f2add99005fba9ec4bd7
jLeibur <jazz_leibur@hotmail.com>
2023-03-24 06:10:33 -0400

    full CRUD completed

--- app.js
+++ app.js
@@ -34,6 +34,19 @@ function sortCategories(categoryArray) {
     categoryArray.sort((toDo1, toDo2) => toDo1.completeBy - toDo2.completeBy || toDo1.priority - toDo2.priority);
 }
 
+function defaultDate(toDoItem) {
+    let day = toDoItem.completeBy.getUTCDate();
+    let month = (toDoItem.completeBy.getUTCMonth() + 1);
+    const year = toDoItem.completeBy.getUTCFullYear();
+    if(month < 10) {
+        month = `0${month}`
+    }
+    if (day < 10) {
+        day = `0${day}`
+    }
+    return `${year}-${month}-${day}`;
+  };
+
 
 app.get('/', async (req, res) => {
     const workToDos = await toDoItem.find({category: "work"});
@@ -42,7 +55,7 @@ app.get('/', async (req, res) => {
     sortCategories(personalToDos);
     const schoolToDos = await toDoItem.find({category: "school"});
     sortCategories(schoolToDos);
-    res.render('index', {workToDos, personalToDos, schoolToDos, formatDate});
+    res.render('index', {workToDos, personalToDos, schoolToDos, formatDate, defaultDate});
 });
 
 app.post('/', async (req, res) => {
@@ -52,13 +65,15 @@ app.post('/', async (req, res) => {
     res.redirect('/');
 })
 
-app.delete('/', async (req, res) => {
-    await toDoItem.findByIdAndDelete(req.body.complete);
+app.delete('/:id', async (req, res) => {
+    await toDoItem.findByIdAndDelete(req.params.id);
     res.redirect('/');
 })
 
-app.patch('/', async (req, res) => {
-
+app.patch('/:id', async (req, res) => {
+    const updates = req.body;
+    await toDoItem.findByIdAndUpdate(req.params.id, {$set: updates});
+    res.redirect('/');
 })
 
 app.listen(port, () => {
