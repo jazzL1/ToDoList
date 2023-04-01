61273c67dde45d9054961c6cb51552b1455ed64b
jLeibur <jazz_leibur@hotmail.com>
2023-04-01 17:18:54 -0400

    changed port to default (80)

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = 3000;
+const port = 80;
 const {toDoItem, user} = require('./schemas.js');
 const isLoggedIn = require('./middleware.js');
 const catchAsync = require('./utilities/catchAsync.js');
